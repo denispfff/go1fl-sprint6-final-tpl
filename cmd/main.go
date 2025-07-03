cbfe567b184d88580ea67eb78cde067a2daaa96d
Panfilov Denis <panfilov.denis96@gmail.com>
2025-07-04 00:08:24 +0300

    First commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	mainLogger := log.New(
+		os.Stdout,
+		"server: ",
+		log.LstdFlags,
+	)
+	srv := server.New(mainLogger)
+
+	if err := srv.HttpServer.ListenAndServe(); err != nil {
+		mainLogger.Fatal(err)
+	}
 }
